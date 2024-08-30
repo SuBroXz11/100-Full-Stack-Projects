68f3b749cd9b4617c749e65ca41d56b41b3d26e1
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-08-30 17:53:30 +0545

    Project 1 Completed

---
+++ Project 1/Readme.md
@@ -0,0 +1,2 @@
+Project Description:
+Python script that prints "Hello, World!" to the console.
---
+++ Project 1/hello_world.py
@@ -0,0 +1,9 @@
+# Constants
+greeting = '"Hello, World!"'
+
+# Function definition
+def print_hello_world():
+    print(greeting)
+
+# Execution
+print_hello_world()
