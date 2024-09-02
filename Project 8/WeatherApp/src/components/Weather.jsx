bdd12fd477731537879b6478652ec84e2cc46a63
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-02 14:32:35 +0545

    API integration completed

--- Project 8/WeatherApp/src/components/Weather.jsx
+++ Project 8/WeatherApp/src/components/Weather.jsx
@@ -1,7 +1,51 @@
+import { useState, useEffect } from "react";
 import "./Weather.css";
 import * as images from '../assets';
 
 const Weather = () => {
+    const [weatherData, setWeatherData] = useState(false);
+    console.log(import.meta.env.VITE_API_KEY);
+
+    // Icon codes according to the API docs
+    const allIcons = {
+        "01d": images.clearIcon,
+        "01n": images.clearIcon,
+        "02d": images.cloudIcon,
+        "02n": images.cloudIcon,
+        "03d": images.cloudIcon,
+        "03n": images.cloudIcon,
+        "04d": images.drizzleIcon,
+        "04n": images.drizzleIcon,
+        "09d": images.rainIcon,
+        "09n": images.rainIcon,
+        "10d": images.rainIcon,
+        "10n": images.rainIcon,
+        "13d": images.snowIcon,
+        "13n": images.snowIcon,
+    }
+
+    const search = async (city) => {
+        try {
+            // adding units as metric will give the value of temp in degrees as per API docs
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_API_KEY}`
+            const response = await fetch(url);
+            const data = await response.json();
+            console.log(data);
+            const icon = allIcons[data.weather[0].icon] || images.clearIcon;
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp), // for removing decimal value from our temperature
+                location: data.name,
+                icon: icon
+            })
+        } catch (error) {
+
+        }
+    }
+    useEffect(() => {
+        search("Kathmandu");
+    }, [])
     return (
         <div className="weather">
             <div className="search-bar">
@@ -9,15 +53,15 @@ const Weather = () => {
                 <img src={images.searchIcon} alt="Search Icon" />
             </div>
 
-            <img src={images.clearIcon} alt="Clear Icon" className="weather-icon" />
-            <p className="temperature">16°c</p>
-            <p className="location">London</p>
+            <img src={weatherData.icon} alt="Clear Icon" className="weather-icon" />
+            <p className="temperature">{weatherData.temperature}°c</p>
+            <p className="location">{weatherData.location}</p>
 
             <div className="weather-data">
                 <div className="col">
                     <img src={images.humidityIcon} alt="Humidity Icon" />
                     <div>
-                        <p>91 %</p>
+                        <p>{weatherData.humidity} %</p>
                         <span>Humidity</span>
                     </div>
                 </div>
@@ -25,7 +69,7 @@ const Weather = () => {
                 <div className="col">
                     <img src={images.windIcon} alt="Wind Speed Icon" />
                     <div>
-                        <p>3.5 Km/h</p>
+                        <p>{weatherData.windSpeed} Km/h</p>
                         <span>Wind Speed</span>
                     </div>
                 </div>
