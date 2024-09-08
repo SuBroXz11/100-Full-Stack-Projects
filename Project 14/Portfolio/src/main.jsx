b078b9284f0c237170093cbfd9d1af7dc3a9535d
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-08 15:10:47 +0545

    original portfolio template added

--- Project 14/Portfolio/src/main.jsx
+++ Project 14/Portfolio/src/main.jsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { ContextProvider } from './Context/Context.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
+  </React.StrictMode>,
 )
