e74ac48bc05676b034b9aa48fabfe06d9f8e2095
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-10 16:38:05 +0545

    medium level project created

--- Project 20/MemoryGame/src/App.jsx
+++ Project 20/MemoryGame/src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Board from './components/Board'
+import MemoryGame from './components/MemoryGame'
 
 const App = () => {
   return (
     <>
-      <h1 className="text-center mt-5 text-3xl">Memory Game Z</h1>
+      <h1 className="text-center mt-5 text-3xl animate-pulse">Memory Game</h1>
       <div className='flex justify-center'>
-        <Board />
+        <MemoryGame />
       </div>
     </>
   )
--- Project 20/MemoryGame/src/components/Card.jsx
+++ Project 20/MemoryGame/src/components/Card.jsx
@@ -1,14 +1,17 @@
 const Card = ({ id, image, isFlipped, onClick }) => {
     return (
         <div
-            className={`card w-20 h-28 bg-base-200 rounded-lg shadow-md m-2 cursor-pointer ${isFlipped ? 'flipped' : ''}`}
+            className={`flip-container w-20 h-28 rounded-lg shadow-md m-2 cursor-pointer ${isFlipped ? 'flipped' : ''}`}
             onClick={() => onClick(id)}
         >
-            {isFlipped ? (
-                <img className="w-full h-full object-cover rounded-lg" src={image} alt="card" />
-            ) : (
-                <div className="card-back w-full h-full bg-primary rounded-lg"></div>
-            )}
+            <div className="flipper">
+                <div className="flip-front card w-full h-full object-cover rounded-lg">
+                    <div className="card-back w-full h-full bg-primary rounded-lg"></div>
+                </div>
+                <div className="flip-back w-full h-full">
+                    <img className="w-full h-full object-cover rounded-lg" src={image} alt="card" />
+                </div>
+            </div>
         </div>
     );
 };
