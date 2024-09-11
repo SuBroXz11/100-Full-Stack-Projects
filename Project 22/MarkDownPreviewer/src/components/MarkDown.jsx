57deada21ef72521e5519f995168326b349bfe31
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-11 13:23:31 +0545

    minor changes

--- Project 22/MarkDownPreviewer/src/components/MarkDown.jsx
+++ Project 22/MarkDownPreviewer/src/components/MarkDown.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import ReactMarkdown from 'react-markdown';
 
 const MarkDown = () => {
     const [markdown, setMarkdown] = useState('# Markdown Preview \n\n## Subheading\n\n ![alt text](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT904XZ8kpOic417aETQDdVWE2u5LpQ6XJhNQ&s) \n\n---');
+    const inputRef = useRef();
+    useEffect(() => {
+        inputRef.current.focus();
+    })
+
 
     return (
         <main className="h-screen bg-base-200 p-8">
@@ -11,14 +16,15 @@ const MarkDown = () => {
                 {/* Text Area Section */}
                 <div className="w-1/2 p-4 h-full">
                     <textarea
-                        className="textarea textarea-bordered w-full h-full p-4 resize-none text-white"
+                        className="textarea textarea-bordered w-full h-full p-4 resize-none text-white bg-slate-600"
                         value={markdown}
                         onChange={(e) => setMarkdown(e.target.value)}
+                        ref={inputRef}
                     ></textarea>
                 </div>
                 {/* Preview Section */}
                 <div className="w-1/2 p-4 bg-white rounded-lg shadow-lg h-full overflow-auto mt-4">
-                    <article className="prose lg:prose-xl max-w-none">
+                    <article className="lg:prose-xl max-w-none text-black">
                         <ReactMarkdown>{markdown}</ReactMarkdown>
                     </article>
                 </div>
