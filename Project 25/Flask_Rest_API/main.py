2c15f4468c6d493e2e8674d52688f46902cd5fbd
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-15 13:12:01 +0545

    Armstrong number checking Flask program learnt

--- Project 25/Flask_Rest_API/main.py
+++ Project 25/Flask_Rest_API/main.py
@@ -5,6 +5,23 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+@app.route('/armstrong/<int:n>')
+def armstrong(n):
+    sum=0
+    order=len(str(n))
+    copy_n=n
+    while(n>0):
+        digit=n%10
+        sum+=digit ** order
+        n=n//10
+        
+    if(sum == copy_n):
+        print(f"{copy_n} is an armstrong")
+        return "True"
+    else:
+        print(f"{copy_n} is not an armstrong number")
+        return "False"
+
 if __name__ == '__main__':
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8080) # port manually added because default was showing bug
 
