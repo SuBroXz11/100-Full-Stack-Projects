09c67b4e69c1e8f9bfb5aa8ab9ba08e793a3a888
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-17 16:49:29 +0545

    screens and required libraries added

--- Project 28/ReactAuth/src/App.jsx
+++ Project 28/ReactAuth/src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Login, Signup, Dashboard } from './screens';
+
+function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
