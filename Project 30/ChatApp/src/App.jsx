f048d9dbbb489e24fae86b10e55ab5b2f9ac9c7a
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-23 11:51:08 +0545

    auth redirection implemented

--- Project 30/ChatApp/src/App.jsx
+++ Project 30/ChatApp/src/App.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import { useState, useRef } from 'react'
 import { Auth } from './components/Auth'
 
+import Cookies from 'universal-cookie'
+const cookies = new Cookies()
+
 const App = () => {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
+  const [room, setRoom] = useState(null)
+  const roomInputRef = useRef(null)
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth setIsAuth={setIsAuth} />
+      </div>
+    )
+  }
   return (
     <div>
-      <Auth />
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div className="room">
+          <label htmlFor="">Enter Room Name: </label>
+          <input type="text" ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
+        </div>
+      )}
     </div>
   )
 }
--- Project 30/ChatApp/src/components/Auth.jsx
+++ Project 30/ChatApp/src/components/Auth.jsx
@@ -3,12 +3,17 @@ import { signInWithPopup } from 'firebase/auth'
 
 import Cookies from 'universal-cookie'
 const cookies = new Cookies()
-export const Auth = () => {
+export const Auth = ({ setIsAuth }) => {
     const signInWithGoogle = async () => {
-        const result = await signInWithPopup(auth, provider);
-        // console.log(result);
-        cookies.set("auth-token", result.user.refreshToken)
+        try {
+            const result = await signInWithPopup(auth, provider);
+            // console.log(result);
+            cookies.set("auth-token", result.user.refreshToken)
+            setIsAuth(true)
+        } catch (error) {
+            console.log(error);
 
+        }
     }
     return (
         <div className="auth">
