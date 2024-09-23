8592812645bd1f383be09bf063d0722570a30dfb
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-23 10:45:48 +0545

    firebase and universal-cookie added

---
+++ Project 30/ChatApp/src/firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBk_lDQC9t_uw2v38riqDpMzJVcl1TsJDQ",
+  authDomain: "reactchatapp-5f7f6.firebaseapp.com",
+  projectId: "reactchatapp-5f7f6",
+  storageBucket: "reactchatapp-5f7f6.appspot.com",
+  messagingSenderId: "322943089860",
+  appId: "1:322943089860:web:d1179661de612a067c5d91",
+  measurementId: "G-SGYY1H213J",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
