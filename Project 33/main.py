9d65fa92f1cd1304237cf72d841e02ef7ae152de
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-27 11:15:36 +0545

    database connection successful with pymyysql

--- Project 33/main.py
+++ Project 33/main.py
@@ -5,22 +5,21 @@ from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object(Config)  # Load configuration from Config object
     register_resources(app)
     register_extensions(app)
     return app
 
 def register_extensions(app):
     db.init_app(app)
-    migrate=Migrate(app, db)
+    migrate = Migrate(app, db)  # Initialize Flask-Migrate
 
 def register_resources(app):
     # homepage
     @app.route('/', methods=['GET'])
     def home():
         return render_template('index.html')
-    
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app = create_app()
     app.run('127.0.0.1', port=8080)
