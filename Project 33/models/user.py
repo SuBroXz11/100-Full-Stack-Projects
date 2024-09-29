6ca4b0a23fe81fde8c769e2bb02e0d6db888a2bd
Subrat Karmacharya <ksubrat22@tbc.edu.np>
2024-09-29 11:12:16 +0545

    new migrations migrated

--- Project 33/models/user.py
+++ Project 33/models/user.py
@@ -1,6 +1,6 @@
 from extensions import db
 
-class User(db.model):
+class User(db.Model):
     __tablename__='users'
     id =db.Column (db.Integer, primary_key=True)
     name= db.Column(db.String(80), nullable=False, unique=False)
--- Project 33/routes.py
+++ Project 33/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
-from controllers import add_user_function
+from controllers.users import add_user_function
+import sys
 
 main = Blueprint('main', __name__) #routename = main
 
@@ -10,4 +11,5 @@ def home():
 @main.route('/adduser', methods=['GET', 'POST'])
 def add_user():
     data=add_user_function()
-    return render_template('adduser.html')
+    print(data, file=sys.stderr)
+    return render_template('adduser.html', data=data)
